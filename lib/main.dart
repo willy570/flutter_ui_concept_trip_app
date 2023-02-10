f5ba84bd8e72ab7afa6e30b10ee86284c0f82e92
kaeloo95 <patrick.kamelan@ngser.com>
2023-02-10 00:30:33 +0000

    update:change title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Trip App',
+      title: 'Ivoire Trip',
       theme: ThemeData(
         fontFamily: 'Nunito',
         primarySwatch: Colors.blue,
